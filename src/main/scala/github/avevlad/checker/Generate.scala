c246148bc39ec8f5df61ebcad045e49601a82c39
AveVlad <dev@vld.me>
2014-07-21 21:33:25 +0400

    add Generate object

--- src/main/scala/github/avevlad/checker/Generate.scala
+++ src/main/scala/github/avevlad/checker/Generate.scala
@@ -1,7 +1,20 @@
 package github.avevlad.checker
 
+import java.io._
+
 object Generate {
   def main(args: Array[String]) {
-    println("123")
+    val writer = new PrintWriter(new File("D:/test.txt"))
+    val untilNumber = 36 * 36 * 36
+    for (x <- 36 * 36 until untilNumber) {
+      val base36: String = java.lang.Long.toString(x, 36)
+      var writeString = base36 + "\n"
+      if (x == untilNumber - 1) {
+        writeString = base36
+      }
+      writer.write(writeString)
+      println(x, "     -     " + base36)
+    }
+    writer.close()
   }
 }
